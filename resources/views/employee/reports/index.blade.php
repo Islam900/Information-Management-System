e8ea25b7e8c06ed5b7b295795e3a5f75e4f59d6b
Mahammad Hazarkhanli <mahammad.hazarkhanli@icta.az>
2024-05-01 15:15:32 +0400

    delete, add functionality to weekly report; detecting exact element while mutation

--- resources/views/employee/reports/index.blade.php
+++ resources/views/employee/reports/index.blade.php
@@ -34,10 +34,37 @@
         }
 
         .container_dragula{
-            height: 100%;
+            min-height: 50px;
+            cursor: pointer;
         }
 
-        .container_dragula div {
+        .container_dragula > div {
+            display: flex;
+            align-items: center;
+        }
+
+        .container_dragula button {
+            flex: 1 1 10%;
+            padding: 12px 24px;
+            border: 1px solid #e9e9e9;
+            border-radius: 4px;
+            background-color: #ff4545;
+            font-size: 16px;
+            text-align: center;
+            color: #fff;
+            font-weight: 400;
+            cursor: pointer;
+            transition: all 0.2s ease;
+            margin-right: 10px;
+        }
+
+        .container_dragula button:hover{
+            border-color: #ffa2a2;
+            background-color: #ff0000;
+        }
+
+        .container_dragula div.list_item {
+            flex: 1 1 90%;
             margin: 10px;
             padding: 10px;
             background-color: rgb(225 225 225 / 52%);
@@ -52,6 +79,50 @@
 
         .container_dragula:nth-child(odd) {
             background-color: rgba(0, 0, 0, 0.2);
+        }
+
+        .wrapper_inside .input_section{
+            display: flex;
+            align-items: center;
+            gap: 16px;
+            padding: 12px 8px;
+        }
+
+        .input_section input{
+            flex: 1 1 80%;
+            padding: 12px;
+            border: 1px solid #bdbdbd;
+            border-radius: 4px;
+            transition: border-color 0.2s ease;
+            font-size: 14px;
+        }
+
+        .input_section input:focus-visible{
+            outline: none;
+            border-color: #605e5e;
+        }
+
+        .input_section button{
+            flex: 1 1 20%;
+            padding: 12px 24px;
+            border: 1px solid #e9e9e9;
+            border-radius: 4px;
+            background-color: #918d8d;
+            font-size: 17px;
+            text-align: center;
+            color: #fff;
+            font-weight: 400;
+            cursor: pointer;
+            transition: all 0.2s ease;
+        }
+
+        button:focus-visible{
+            outline: none;
+        }
+
+        .input_section button:hover{
+            border-color: #524f4f;
+            background-color: #a9a4a4;
         }
     </style>
     <div class="row mb-4">
@@ -72,12 +143,21 @@
                         <div class="wrapper_inside" id="wrapper">
                             <div class="left_container">
                                 <div class="header">Görüləcək işlər</div>
+
                                 <div id="left" class="container_dragula">
                                 @foreach($reports as $report)
                                     @foreach($report->reports_subjects->where('status', 0) as $subject_key => $subject)
-                                    <div>{{ $subject->project_name ?? NULL }} - {{ $subject->subject}}</div>
+                                    <div>
+                                        <div class="list_item"><p data-id="{{ $subject->id }}">{{ $subject->project_name ?? NULL }} - {{ $subject->subject}}</p></div>
+                                        <button>Sil</button>
+                                    </div>
                                     @endforeach
                                 @endforeach
+                                </div>
+
+                                <div class="input_section">
+                                    <input type="text">
+                                    <button type="button">Əlavə et</button>
                                 </div>
                             </div>
 
@@ -181,7 +261,24 @@
 
             // Create a callback function to execute when mutations are observed
             const callback = function(mutationsList, observer) {
+                
                 for (const mutation of mutationsList) {
+
+                    if (mutation.type === 'childList') {
+                        
+                        if (mutation.addedNodes.length > 0) {
+                            mutation.addedNodes.forEach(node => {
+                                const data_id = node.querySelector('p').getAttribute('data-id');
+                            });
+                        }
+                        
+                        if (mutation.removedNodes.length > 0) {
+                            mutation.removedNodes.forEach(node => {
+                                const data_id = node.querySelector('p').getAttribute('data-id');
+                            });
+                        }
+                    }
+
                     if(mutation.type === 'attributes'){
                         console.log('something changed inside of the element 🦝')
                     }
