3be2734f6767a0db5bafa69b78bf4db23a39d74c
toghrulvg <toghrulvg@code.edu.az>
2024-05-17 17:09:18 +0400

    asset request deleting added

--- resources/views/employee/assets-requests/index.blade.php
+++ resources/views/employee/assets-requests/index.blade.php
@@ -4,11 +4,11 @@
     <div class="card">
         <div class="card-header">
             <div class="d-flex justify-content-between align-items-center">
-                <h3>Mal-material sorğusu</h3>
+                <h3>Mal-material sorğuları</h3>
                 <button class="btn btn-success new-assets-requests-button">
-                            <span>
-                                <i class="nav-icon i-Add-File"></i>
-                            </span>
+                <span>
+                    <i class="nav-icon i-Add-File"></i>
+                </span>
                     Yeni mal-material sorğusu
                 </button>
             </div>
@@ -34,7 +34,12 @@
                         <td>Gözləyir</td>
                         <td>{{ $asset->created_at }}</td>
                         <td>
-
+                            <button class="btn btn-danger delete-assets-request" data-id="{{ $asset->id }}">
+                            <span>
+                                <i class="nav-icon i-Delete-File"></i>
+                            </span>
+                                Sorğunu sil
+                            </button>
                         </td>
                     </tr>
                 @endforeach
@@ -48,9 +53,8 @@
 
 @section('js')
     <script>
-        $('document').ready(function () {
+        $(document).ready(function () {
             $('.new-assets-requests-button').on('click', function (e) {
-
                 Swal.fire({
                     title: "Yeni mal-material sorğusu yaradın",
                     input: "text",
@@ -62,10 +66,10 @@
                     showLoaderOnConfirm: true,
                     preConfirm: async (assets_content) => {
                         $.ajax({
-                            url: "{{route('employee.assets-requests.store')}}",
+                            url: "{{ route('employee.assets-requests.store') }}",
                             method: "POST",
                             data: {
-                                "_token": "{{csrf_token()}}",
+                                "_token": "{{ csrf_token() }}",
                                 "assets_content": assets_content
                             },
                             success: function (response) {
@@ -79,10 +83,56 @@
                                     });
                                 }
                             }
-                        })
+                        });
                     },
                     allowOutsideClick: () => !Swal.isLoading()
-                })
+                });
+            });
+
+            $('.delete-assets-request').on('click', function (e) {
+                e.preventDefault();
+                let requestId = $(this).data('id');
+
+                Swal.fire({
+                    title: "Sorğunu silmək istədiyinizə əminsiniz?",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonText: "Bəli, silin",
+                    cancelButtonText: "Xeyr, ləğv et"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        $.ajax({
+                            url: "{{ route('employee.assets-requests.destroy', '') }}/" + requestId,
+                            method: "DELETE",
+                            data: {
+                                "_token": "{{ csrf_token() }}"
+                            },
+                            success: function (response) {
+                                if (response.status == 200) {
+                                    Swal.fire({
+                                        title: response.message,
+                                        icon: "success"
+                                    }).then(() => {
+                                        location.reload();
+                                    });
+                                } else {
+                                    Swal.fire({
+                                        title: "Xəta baş verdi",
+                                        text: response.message,
+                                        icon: "error"
+                                    });
+                                }
+                            },
+                            error: function (xhr) {
+                                Swal.fire({
+                                    title: "Xəta baş verdi",
+                                    text: xhr.responseText,
+                                    icon: "error"
+                                });
+                            }
+                        });
+                    }
+                });
             });
         });
     </script>
