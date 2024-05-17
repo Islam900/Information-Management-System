2b60a9de95b76e532a8a635955ca5c0f805e19d6
cavid <islam.baghirzada@icta.az>
2024-05-17 17:51:31 +0400

    show deleted data for departments

--- app/Http/Controllers/ITD/ITDAssetsRequestsController.php
+++ app/Http/Controllers/ITD/ITDAssetsRequestsController.php
@@ -10,8 +10,14 @@ use App\Models\AssetsRequestsDetails;
 class ITDAssetsRequestsController extends Controller
 {
     public function index() {
-        print_r("1");
-        $assets= AssetsRequests::with('user')->get();
+        $current_user_id = Auth::user()->assets_requests_id;
+        $assets = AssetsRequests::with('user')
+            ->whereHas('assets_requests_details', function ($query) use ($current_user_id) {
+                $query->where('assets_requests_id', '<', $current_user_id);
+            })
+            ->get();
+        dd($assets);
+
         return view('itd-leader.assets-requests.index', compact('assets'));
     }
 
